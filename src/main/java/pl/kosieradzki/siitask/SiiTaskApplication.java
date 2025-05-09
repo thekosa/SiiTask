4c16cd82ffdca9e8e383cc64f79aba3df7f4740e
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2025-05-09 15:13:58 +0200

    init

---
+++ src/main/java/pl/kosieradzki/siitask/SiiTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.kosieradzki.siitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiiTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kosieradzki/siitask/SiiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kosieradzki.siitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
