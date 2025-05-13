aac6cfc0664e9e476c2255997d5aac371e3430c8
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2025-05-13 21:46:37 +0200

    unification eventController

--- src/main/java/pl/kosieradzki/siitask/controller/EventController.java
+++ src/main/java/pl/kosieradzki/siitask/controller/EventController.java
@@ -2,25 +2,27 @@ package pl.kosieradzki.siitask.controller;
 
 import org.springframework.web.bind.annotation.*;
 import pl.kosieradzki.siitask.model.Event;
+import pl.kosieradzki.siitask.repo.BoxRepo;
 import pl.kosieradzki.siitask.repo.EventRepo;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/events")
 public class EventController {
-    private final EventRepo repository;
+    private final EventRepo eventRepo;
 
-    public EventController(EventRepo repository) {
-        this.repository = repository;
+    public EventController(EventRepo eventRepo) {
+        this.eventRepo = eventRepo;
     }
 
     @PostMapping("/new")
     public Event createEvent(@RequestBody Event event) {
-        return repository.save(event);
+        return eventRepo.save(event);
     }
 
     @GetMapping
     public List<Event> getAll() {
-        return repository.findAll();
+        return eventRepo.findAll();
     }
 }
