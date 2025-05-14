1ff842710e63e2d397ee339a525e08d72877e903
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2025-05-14 12:15:36 +0200

    new donation feature added

--- src/main/java/pl/kosieradzki/siitask/controller/DonationController.java
+++ src/main/java/pl/kosieradzki/siitask/controller/DonationController.java
@@ -1,10 +1,9 @@
 package pl.kosieradzki.siitask.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.kosieradzki.siitask.model.Box;
 import pl.kosieradzki.siitask.model.Donation;
+import pl.kosieradzki.siitask.repo.BoxRepo;
 import pl.kosieradzki.siitask.repo.DonationRepo;
 
 import java.util.List;
@@ -13,9 +12,11 @@ import java.util.List;
 @RequestMapping("/donations")
 public class DonationController {
     private final DonationRepo donationRepo;
+    private final BoxRepo boxRepo;
 
-    public DonationController(DonationRepo donationRepo) {
+    public DonationController(DonationRepo donationRepo, BoxRepo boxRepo) {
         this.donationRepo = donationRepo;
+        this.boxRepo = boxRepo;
     }
 
     @GetMapping
@@ -26,5 +27,12 @@ public class DonationController {
     @GetMapping("/{boxId}")
     public List<Donation> getDonationsByBoxId(@PathVariable int boxId) {
         return donationRepo.getDonationsByBoxId(boxId);
+    }
+
+    @PostMapping("/{boxId}/new")
+    public Donation createDonation(@PathVariable int boxId,@RequestBody Donation donation) {
+        Box box = boxRepo.findById(boxId).orElseThrow(() -> new RuntimeException("Box not found"));
+        donation.setBox(box);
+        return donationRepo.save(donation);
     }
 }
